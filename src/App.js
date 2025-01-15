c6c3855b01d08fa2ef31171b5c74a92249afacb3
FranciscoLattaro <lpdcmelo@gmail.com>
2025-01-15 12:36:34 -0300

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<HomeView />} />
+        <Route path="/" element={<HomeView />} />
       </Routes>
     </Router>
   );

2f6ce63252b467ece80dda803579d430ccccbe41
FranciscoLattaro <lpdcmelo@gmail.com>
2025-01-17 22:44:48 -0300

    .

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -13,12 +13,14 @@ const services = [
     {
         title: 'Gestión de Entregas sin Esfuerzo',
         description: 'Nos encargamos de tus envíos para que te enfoques en crecer.',
-        image: 'https://img.freepik.com/foto-gratis/hombre-entrega-paquetes_23-2148869418.jpg?t=st=1736953752~exp=1736957352~hmac=bf758d9ef7fda738df1fa55a20d0c719e4d9c3501a7a28c43309eb4d019af6bd&w=1060'
+        image: 'moto.png',
+        button: true,
+        buttonValue: 'Haz click para saber más!'
     },
     {
         title: 'Soporte Técnico Continuo',
         description: 'Aseguramos el éxito de tu tienda online.',
-        image: 'https://img.freepik.com/foto-gratis/mujer-joven-que-trabaja-su-taller-invencion-creativa_23-2149067209.jpg?t=st=1736954672~exp=1736958272~hmac=619b31dd0ffdd0a75afc7da459d58661a6fbd77b203da31434beee9391c2fd51&w=1060'
+        image: 'service.png'
     }
 ];
 

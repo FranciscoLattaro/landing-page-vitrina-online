bafede3266e4728784d2de8d3d0f721e4aa355e1
FranciscoLattaro <lpdcmelo@gmail.com>
2025-01-18 11:25:22 -0300

    coso

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -4,6 +4,20 @@ import './Carrousel.css';
 
 const services = [
     {
+        title: 'Crea tu Tienda Online con Shopify',
+        description: 'Tu tienda digital lista para empezar a vender.',
+        image: '/shopify.png',
+        button: true,
+        buttonValue: 'Haz click para ver ejemplos!'
+    },
+    {
+        title: 'Gestión de Entregas sin Esfuerzo',
+        description: 'Nos encargamos de tus envíos para que te enfoques en crecer.',
+        image: 'moto.png',
+        button: true,
+        buttonValue: 'Haz click para saber más!'
+    },
+    {
         title: 'Soporte Técnico Continuo',
         description: 'Aseguramos el éxito de tu tienda online.',
         image: 'service.png'
@@ -14,20 +28,6 @@ const services = [
         image: 'gestoria.jpg',
         button: true,
         buttonValue: 'Haz click para saber más!'
-    },
-    {
-        title: 'Gestión de Entregas sin Esfuerzo',
-        description: 'Nos encargamos de tus envíos para que te enfoques en crecer.',
-        image: 'moto.png',
-        button: true,
-        buttonValue: 'Haz click para saber más!'
-    },
-    {
-        title: 'Crea tu Tienda Online con Shopify',
-        description: 'Tu tienda digital lista para empezar a vender.',
-        image: '/shopify.png',
-        button: true,
-        buttonValue: 'Haz click para ver ejemplos!'
     }
 ];
 
@@ -46,6 +46,7 @@ const itemTemplate = (service) => {
                         onClick={() => alert('Ejemplo de acción para el botón!')}
                     />
                 )}
+                <p className='mt-3 desliza'>Desliza para ver más!</p>
             </div>
         </div>
     );
@@ -56,11 +57,11 @@ const ServiceCarousel = () => {
         <div className="carrousel">
             <Carousel
                 value={services}
-                autoplayInterval={3000}
                 circular={true}
                 numVisible={1}
                 numScroll={1}
                 itemTemplate={itemTemplate}
+                autoplayInterval={5000}
             />
         </div>
     );

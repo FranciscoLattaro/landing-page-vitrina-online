6cef7f7d7325e7fe02e6137779bda5c36854ed1d
FranciscoLattaro <lpdcmelo@gmail.com>
2025-01-18 11:26:50 -0300

    .

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -4,20 +4,6 @@ import './Carrousel.css';
 
 const services = [
     {
-        title: 'Crea tu Tienda Online con Shopify',
-        description: 'Tu tienda digital lista para empezar a vender.',
-        image: '/shopify.png',
-        button: true,
-        buttonValue: 'Haz click para ver ejemplos!'
-    },
-    {
-        title: 'Gestión de Entregas sin Esfuerzo',
-        description: 'Nos encargamos de tus envíos para que te enfoques en crecer.',
-        image: 'moto.png',
-        button: true,
-        buttonValue: 'Haz click para saber más!'
-    },
-    {
         title: 'Soporte Técnico Continuo',
         description: 'Aseguramos el éxito de tu tienda online.',
         image: 'service.png'
@@ -28,6 +14,20 @@ const services = [
         image: 'gestoria.jpg',
         button: true,
         buttonValue: 'Haz click para saber más!'
+    },
+    {
+        title: 'Gestión de Entregas sin Esfuerzo',
+        description: 'Nos encargamos de tus envíos para que te enfoques en crecer.',
+        image: 'moto.png',
+        button: true,
+        buttonValue: 'Haz click para saber más!'
+    },
+    {
+        title: 'Crea tu Tienda Online con Shopify',
+        description: 'Tu tienda digital lista para empezar a vender.',
+        image: '/shopify.png',
+        button: true,
+        buttonValue: 'Haz click para ver ejemplos!'
     }
 ];
 
